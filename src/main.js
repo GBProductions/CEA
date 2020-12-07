44e58e6ff4ae1631a5ffa54cc6d0b690aef24d92
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-06 19:17:42 -0800

    Create clearFields function.

--- src/main.js
+++ src/main.js
@@ -5,16 +5,19 @@ import './css/styles.css';
 import { Country } from './js/api.js';
 //import { convertAUD, convertEUR, convertGBP, convertJPY, convertCHF, notCurrency, statusError } from './js/international';
 
+function clearFields() {
+  $('#usdInput').val("");
+  $('#currencyConvert').val("");
+}
 
 
 $('document').ready(function() {
   $('#submitUSD').click(function() {
     event.preventDefault();
     const usDollar = parseInt($('#usdInput').val());
-    $('#usdInput').val("");
     const currency = $('#currencyConvert').val();
-    $('#currencyConvert').val("");
-
+    clearFields();
+    
     Country.currency();
 
     console.log(Country.currency());
