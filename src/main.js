edfe16534a1b4e8fbc117e34b8ba5d040d7775ee
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-06 21:28:52 -0800

    Add currency selection in main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Country from './js/api';
-//import { convertAUD, convertEUR, convertGBP, convertJPY, convertCHF, notCurrency, statusError } from './js/international';
+import { convertAUD, convertEUR, convertGBP, convertJPY, convertCHF, notCurrency, statusError } from './js/international';
 
 function clearFields() {
   $('#usdInput').val("");
   $('#currencyConvert').val("");
 }
+
 
 $('document').ready(function() {
   $('#submitUSD').click(function() {
@@ -21,8 +22,25 @@ $('document').ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-
-      $('.showAUD').text("Your $" + usDollar + " is equal to " + (body.conversion_rates.AUD * usDollar) + " " + currency);
     });
+
+    if (currency === "AUD") {
+      convertAUD(response, usDollar);
+    } else if (currency === "EUR") {
+      convertEUR(response, usDollar);
+    } else if (currency === "GBP") {
+      convertGBP(response, usDollar);
+    } else if (currency === "JPY") {
+      convertJPY(response, usDollar);
+    } else if (currency === "CHF") {
+      convertCHF(response, usDollar);
+    } else if ((currency !== "EUR" || "GBP" || "JPY" || "CHF")) {
+      notCurrency(currency);
+    }
+    if (this.status !== 200) {
+      let error = this.status;
+      statusError(error);
+    };
   });
 });
+// $('.showAUD').text("Your $" + usDollar + " is equal to " + (body.conversion_rates.AUD * usDollar) + " " + currency);
