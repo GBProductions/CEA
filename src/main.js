b7395399e180f8081443079d1d402549a12531fd
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-06 21:04:19 -0800

    Hard code USD to AUD.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Country } from './js/api.js';
+import Country from './js/api';
 //import { convertAUD, convertEUR, convertGBP, convertJPY, convertCHF, notCurrency, statusError } from './js/international';
 
 function clearFields() {
@@ -10,90 +10,19 @@ function clearFields() {
   $('#currencyConvert').val("");
 }
 
-function getConversion() {
-  
-}
-
 $('document').ready(function() {
   $('#submitUSD').click(function() {
     event.preventDefault();
     let usDollar = parseInt($('#usdInput').val());
     let currency = $('#currencyConvert').val();
+    let USD = "USD";
     clearFields();
-    let promise = Country.currencyConversion(currency);
-    console.log(promise);
+    let promise = Country.currencyConversion(USD);
 
-    // promise.then(function(response) {
-    // const body = JSON.parse(response);
-    //   function convertAUD(response, usDollar) {
-    //     $('.showAUD').text("Your $" + usDollar + " is equal to " + (response.conversion_rates.AUD * usDollar) + " AUD");
-    //     $('.showEUR').text("");
-    //     $('.showGBP').text("");
-    //     $('.showJPY').text("");
-    //     $('.showCHF').text("");
-    //     $('.showErrors').text("");
-    //     $('.statusError').text("");
-    //   }
-      
-    //   function convertEUR(response, usDollar) {
-    //     $('.showAUD').text("");
-    //     $('.showEUR').text("Your $" + usDollar + " is equal to " + (response.conversion_rates.EUR * usDollar) + " EUR");
-    //     $('.showGBP').text("");
-    //     $('.showJPY').text("");
-    //     $('.showCHF').text("");
-    //     $('.showErrors').text("");
-    //     $('.statusError').text("");
-    //   }
-      
-    //   function convertGBP(response, usDollar) {
-    //     $('.showAUD').text("");
-    //     $('.showEUR').text("");
-    //     $('.showGBP').text("Your $" + usDollar + " is equal to " + (response.conversion_rates.GBP * usDollar) + " GBP");
-    //     $('.showJPY').text("");
-    //     $('.showCHF').text("");
-    //     $('.showErrors').text("");
-    //     $('.statusError').text("");
-    //   }
-      
-    //   function convertJPY(response, usDollar) {
-    //     $('.showAUD').text("");
-    //     $('.showEUR').text("");
-    //     $('.showGBP').text("");
-    //     $('.showJPY').text("Your $" + usDollar + " is equal to " + (response.conversion_rates.JPY * usDollar) + " JPY");
-    //     $('.showCHF').text("");
-    //     $('.showErrors').text("");
-    //     $('.statusError').text("");
-    //   }
-      
-    //   function convertCHF(response, usDollar) {
-    //     $('.showAUD').text("");
-    //     $('.showEUR').text("");
-    //     $('.showGBP').text("");
-    //     $('.showJPY').text("");
-    //     $('.showCHF').text("Your $" + usDollar + " is equal to " + (response.conversion_rates.CHF * usDollar) + " CHF");
-    //     $('.showErrors').text("");
-    //     $('.statusError').text("");
-    //   }
-      
-    //   function notCurrency(currency) {
-    //     $('.showAUD').text("");
-    //     $('.showEUR').text("");
-    //     $('.showGBP').text("");
-    //     $('.showJPY').text("");
-    //     $('.showCHF').text("");
-    //     $('.showErrors').text("The currency " + currency + " doesn't exist!");
-    //     $('.statusError').text("");
-    //   }
-      
-    //   function statusError(error) {
-    //     $('.showAUD').text("");
-    //     $('.showEUR').text("");
-    //     $('.showGBP').text("");
-    //     $('.showJPY').text("");
-    //     $('.showCHF').text("");
-    //     $('.showErrors').text("");
-    //     $('.statusError').text("You have a " + error + " status error!");
-    //   }
-    // });
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+
+      $('.showAUD').text("Your $" + usDollar + " is equal to " + (body.conversion_rates.AUD * usDollar) + " " + currency);
+    });
   });
 });
